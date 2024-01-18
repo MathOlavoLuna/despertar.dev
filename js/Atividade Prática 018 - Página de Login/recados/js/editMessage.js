a73e7ac2854deddd900156b025d1591e98d144e1
Matheus Luna <srmatheus6162@gmail.com>
2024-01-18 15:07:14 -0300

    [MOD] ATT COM BOOTSTRAP

--- js/Atividade Prática 018 - Página de Login/recados/js/editMessage.js
+++ js/Atividade Prática 018 - Página de Login/recados/js/editMessage.js
@@ -49,3 +49,22 @@ async function updateMessage(messageId, editMessage) {
     console.log('Erro ao atualizar recado.')
   }
 }
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
